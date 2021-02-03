ce9c27f2dd0fb7ed712e0b7e260182ee3243c5a8
ablause <46499306+ablause@users.noreply.github.com>
2021-02-03 01:26:44 +0100

    Removing application module

--- packages/server/src/modules/applications/applications.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApplicationsService } from './applications.service';
-import { ApplicationsResolver } from './applications.resolver';
-
-@Module({
-  providers: [ApplicationsService, ApplicationsResolver]
-})
-export class ApplicationsModule {}
